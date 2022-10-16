cb20a2ba3ee12876cbe38f443be0a03dbc0bc339
KARTIK DHIMAN <kartik1919.be20@chitkara.edu.in>
2022-10-16 12:08:40 +0530

    Added my Portfolio

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hi, I am Kartik Dhiman</span> I am a Third-year Computer Science student. <br><br> My interest in computers and how to develop softwares brought me to Chitkara University, where I am working on my BE in Computer Science Engineering."
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
